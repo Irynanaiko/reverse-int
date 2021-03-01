cf59d726ba52570114c9fd010623dff2f282626c
Irynanaiko <irynaiko@ukr.net>
2021-03-01 13:42:12 +0200

    feat: implement revers-int function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    n = n + "";
+
+    if (n > 0) {
+        return n.split("").reverse().join("");
+    } else {
+        return n.slice(1).split("").reverse().join("");
+    }
+};
